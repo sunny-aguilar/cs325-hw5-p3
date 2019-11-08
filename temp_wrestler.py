23f99d03e1c47a420e3fcb91cb56000433f4090b
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-11-07 21:55:09 -0800

    wres

--- temp_wrestler.py
+++ temp_wrestler.py
@@ -2,6 +2,22 @@ import sys
 
 infile = open('wrestler4.txt', 'r')
 
+lineup = {}
+
+# get total wrestlers
+n = int(infile.readline().strip())
+print('# of wrestlers: ', n)
+
+# get wrestlers & assign team
+for wrestler in range(n):
+  wrestler_name = infile.readline().strip()
+  if wrestler == 0:
+    lineup[wrestler_name] = 'babyface'
+  else:
+    lineup[wrestler_name] = 'none'
+
+
+print(lineup)
 
 
 
