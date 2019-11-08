c11431a809f250d3ecaad30c9e244c929e119958
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-11-07 23:39:20 -0800

    wres

--- temp_graph.py
+++ temp_graph.py
@@ -9,12 +9,23 @@ n = int(infile.readline().strip())
 print('# of wrestlers: ', n)
 
 
-for wrestler in range(n):
+for i in range(n):
   wrestler_name = infile.readline().strip()
-  if wrestler == 0:
-    lineup[wrestler_name] = 'babyface'
+  if i == 0:
+    graph[wrestler_name] = ['babyface']
   else:
-    lineup[wrestler_name] = 'none'
+    graph[wrestler_name] = ['none']
+
+print(graph)
+
+
+# get number of rivalries
+rivalries = infile.readline().strip()
+print('# of rivalries: ', rivalries)
+
+for i in range(n-1):
+  line = infile.readline().strip()
+
 
 
 
