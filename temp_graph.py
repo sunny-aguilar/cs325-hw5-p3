873b4249de456fe9a17cda1661568f7481933a5b
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-11-07 23:34:55 -0800

    wres

---
+++ temp_graph.py
@@ -0,0 +1,13 @@
+import sys
+
+infile = open('wrestler4.txt', 'r')
+
+graph = {}           # dictionary to hold wrestlers
+
+# get total wrestlers
+n = int(infile.readline().strip())
+print('# of wrestlers: ', n)
+
+
+
+
