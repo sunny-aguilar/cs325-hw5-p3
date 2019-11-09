909b227a9c0bd978f732add247138947d5b10cd3
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-11-08 22:05:51 -0800

    wres

--- temp_wrestler.py
+++ temp_wrestler.py
@@ -32,10 +32,12 @@ print('# of wrestlers: ', n)
 
 
 # create wrestler nodes and assign alignment
+start_vertex = None
 for wrestler in range(n):
   wrestler_name = infile.readline().strip()
   if wrestler == 0:
     p1 = Wrestler(wrestler_name, 'babyface')
+    start_vertex = wrestler_name
   else:
     p1 = Wrestler(wrestler_name, 'none')
 
@@ -62,11 +64,44 @@ for vertex in graph:
   print(graph[vertex].get_name(), graph[vertex].get_rival())
 
 
-#def BFS_Search(graph):
+def BFS_Search(graph, n, start_vertex):
+  # visited vertices
+  visited = [0 for x in range(n)]
+
+  # create a queue
+  queue = []
+
+  #enque neighbors of start vertex
+  for i in graph[start_vertex].get_rival():
+    queue.append(i)
+    visited[0] = graph[start_vertex].get_name()
+  print('Rivals Queue: ', queue[0])
+  print('Visited: ', visited)
+  print('Team: ', graph[start_vertex].get_team())
+
+  while len(queue) is not 0:
+    # remove node from queue
+    node = queue.pop()
+    print('Node :', node)
+    # check rival teams, exit if the same
+    for i in graph[node].get_rival():
+      print('Rival Name: ', graph[i].get_name())
+      if graph[i].get_team == graph[node].get_team:
+        print('Same teams!')
+        exit('Impossible')
+
+    if 
+    graph[node].set_team('')
+
   # nodes
-  #start = graph[0]
+  # if graph[start_vertex].get_team() is 'babyface':
+  #   print('Babyface found!')
 
 
+
+
+
+BFS_Search(graph, n, start_vertex)
 
 
 
